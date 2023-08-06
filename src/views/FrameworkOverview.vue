ce54e89b1c270853e8c97a9ceae656744f899c8f
Yannick Müller <mail@ynnk.dev>
2023-08-06 14:37:17 +0200

    fixed image issue

--- src/views/FrameworkOverview.vue
+++ src/views/FrameworkOverview.vue
@@ -17,6 +17,23 @@
 
 import FrameworkCard from '@/components/FrameworkCard.vue';
 
+import angular from '@/assets/image/logos/angular.svg';
+import vue from '@/assets/image/logos/vue3.png';
+import tailwind from '@/assets/image/logos/tailwindcss.png';
+import ngrx from '@/assets/image/logos/ngrx.svg';
+
+import springboot from '@/assets/image/logos/spring.png';
+import springsecurity from '@/assets/image/logos/spring_security.png';
+
+import ts from '@/assets/image/logos/typescript.png';
+import js from '@/assets/image/logos/javascript.png';
+import java from '@/assets/image/logos/java.png';
+import python from '@/assets/image/logos/python.png';
+import cpp from '@/assets/image/logos/cpp.png';
+
+import mysql from '@/assets/image/logos/mysql.png';
+import mongodb from '@/assets/image/logos/mongodb.png';
+
 export default {
     name: "FrameworkOverivew",
     components: {
@@ -27,35 +44,35 @@ export default {
             frameworks: [{
                 groupName: "Web Technologies",
                 frameworks: [
-                    { title: "Vue 3", logo: "/src/assets/image/logos/vue3.png", projectUrl: "https://github.com/Yanni8/yanni8-portfolio/" },
-                    { title: "Angular", logo: "/src/assets/image/logos/angular.svg", projectUrl: "https://github.com/bbzblit/stundenplan-ui" },
-                    { title: "NgRx", logo: "/src/assets/image/logos/ngrx.svg", projectUrl: "https://github.com/bbzblit/stundenplan-ui" },
-                    { title: "TailwindCSS", logo: "/src/assets/image/logos/tailwindcss.png", projectUrl: "https://github.com/Yanni8/yanni8-portfolio/" }
+                    { title: "Vue 3", logo: vue, projectUrl: "https://github.com/Yanni8/yanni8-portfolio/" },
+                    { title: "Angular", logo: angular, projectUrl: "https://github.com/bbzblit/stundenplan-ui" },
+                    { title: "NgRx", logo: ngrx, projectUrl: "https://github.com/bbzblit/stundenplan-ui" },
+                    { title: "TailwindCSS", logo: tailwind, projectUrl: "https://github.com/Yanni8/yanni8-portfolio/" }
                 ]
             },
             {
                 groupName: "Backend Technologies",
                 frameworks: [
-                    { title: "Spring Boot", logo: "/src/assets/image/logos/spring.png", projectUrl: "https://github.com/Yanni8/M295" },
-                    { title: "Spring Security", logo: "/src/assets/image/logos/spring_security.png", projectUrl: "https://github.com/Yanni8/M295" }
+                    { title: "Spring Boot", logo: springboot, projectUrl: "https://github.com/Yanni8/M295" },
+                    { title: "Spring Security", logo: springsecurity, projectUrl: "https://github.com/Yanni8/M295" }
 
                 ]
             },
             {
                 groupName: "Programming Languages",
                 frameworks: [
-                    { title: "Type Script", logo: "/src/assets/image/logos/typescript.png", projectUrl: "https://github.com/Yanni8/M294" },
-                    { title: "Java Script", logo: "/src/assets/image/logos/javascript.png", projectUrl: "https://github.com/bbzblit/m293-project" },
-                    { title: "Python", logo: "/src/assets/image/logos/python.png", projectUrl: "" },
-                    { title: "Java", logo: "/src/assets/image/logos/java.png", projectUrl: "https://github.com/Yanni8/M295" },
-                    { title: "C++", logo: "/src/assets/image/logos/cpp.png", projectUrl: "https://github.com/bbzblit/stundenplan-api" }
+                    { title: "Type Script", logo: ts, projectUrl: "https://github.com/Yanni8/M294" },
+                    { title: "Java Script", logo: js, projectUrl: "https://github.com/bbzblit/m293-project" },
+                    { title: "Python", logo: python, projectUrl: "" },
+                    { title: "Java", logo: java, projectUrl: "https://github.com/Yanni8/M295" },
+                    { title: "C++", logo: cpp, projectUrl: "https://github.com/bbzblit/stundenplan-api" }
                 ]
             },
             {
                 groupName: "Databases",
                 frameworks: [
-                    { title: "MySQL", logo: "/src/assets/image/logos/mysql.png", projectUrl: "https://github.com/bbzblit/stundenplan-api"},
-                    { title: "MongoDB", logo: "/src/assets/image/logos/mongodb.png", projectUrl: "https://github.com/bbzblit/m322-backend" },
+                    { title: "MySQL", logo: mysql, projectUrl: "https://github.com/bbzblit/stundenplan-api"},
+                    { title: "MongoDB", logo: mongodb, projectUrl: "https://github.com/bbzblit/m322-backend" },
                 ]
             }
             ]
