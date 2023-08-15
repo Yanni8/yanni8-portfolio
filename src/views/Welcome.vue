f08c93c43bdb7cbe9b9f5935bb925d57b643634c
Yannick Müller <mail@ynnk.dev>
2023-08-15 07:09:28 +0200

    fixed issue on ihone

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="h-[85vh] max-w-[80%] relative mx-auto flex flex-wrap flex-column align-center justify-center">
+    <div class="h-[85vh] max-w-[80%] max-md:pb-16 relative mx-auto flex flex-wrap flex-column align-center justify-center">
         <h1 class="text-5xl">Hey, I'm <span class="font-bold inline">Yannick Müller</span></h1>
         <h2 class="pt-8 text-4xl max-md:mr-auto max-md:min-h-[10rem]">I'm a <span id="placeholder"></span></h2>
-        <div class="absolute bottom-0 animate-bounce">
+        <div class="absolute bottom-0 max-md:bottom-20 animate-bounce">
             <v-btn icon="fas fa-arrow-down" @click="goto()">
             </v-btn>
         </div>
